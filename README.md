3c7c3c4062cf8ed684aed06a3bead794ebec8e90
Toughee <Jimernez.io@gmail.com>
2022-01-04 18:57:40 -0600

    Fodee/002/React_State_Update
    - Updated U.I for app
    - Added shopping cart state into project. Drafted pseudocode for final
    checkout option
    - Updated README file for app

--- README.md
+++ README.md
@@ -1,6 +1,43 @@
 ![App logo](./public/img/fodee_logo.png)
 
-
 # Fodee
 
 Food ordering dashboard app
+
+![Splash page](./public/img/Fodee_Splash.png)
+
+## Why this app as created:
+
+Proof of concept: This app was created as an example of React state, regular expressions and core Javascript functions.
+
+## How to use:
+
+1. Clone repo into desired directory `git clone https://github.com/Toughee/Fodee.git` or `gh repo clone Toughee/Fodee`
+2. Run `npm install` in your terminal to install all app node modules and dependencies
+3. Run `yarn start` in your terminal to run app on http://localhost:3000/
+
+## Testing tools used for app:
+
+-   React Testing library
+-   Jest
+-   Cypress
+-   TravisCI
+-   CircleCI
+
+## Project resources:
+
+-   [Current roadmap for app](https://github.com/Toughee/Fodee/projects/1)
+-   [Wiki page for app](https://github.com/Toughee/Fodee/wiki)
+-   [Documentation for project contributors](https://github.com/Toughee/Fodee/tree/main/docs)
+
+### Completed app tasks:
+
+-   [x] Include CircleCI and Travis-CI inside project
+-   [x] Improve U.I. design for app
+
+### Future app goals:
+
+-   [ ] Fix addition error with float number where sum shows dozens of numbers after float period
+-   [ ] Working checkout button for numbers. With boolean conditionals
+-   [ ] Fix subtraction error where numbers keep subtracting from each state even as state has 0 values inside of it
+-   [ ] Perfect U.X. design for app
--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -1,123 +1,205 @@
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
 import { BsCartFill, BsFillPlusSquareFill } from 'react-icons/bs'
+import { Container } from 'react-grid-system'
 
 import { IoFastFoodSharp } from 'react-icons/io5'
 
-import React from 'react'
-
-import { Container, Row, Col } from 'react-grid-system'
-
 // import Item1_SPS from '../assets/Item1_SPS'
 
+// const addToCar = handleClick
+// add to state.cart array
+// item+
+// price+
+
+const items = [
+    {
+        id: 1,
+        pic: <IoFastFoodSharp />,
+        name: 'Sweet Potato Salad',
+        price: '8.64',
+    },
+    {
+        id: 2,
+        pic: <IoFastFoodSharp />,
+        name: 'Brown Crust Pizza',
+        price: '15.35',
+    },
+    {
+        id: 3,
+        pic: <IoFastFoodSharp />,
+        name: 'Mega Burger with Fries',
+        price: '14.71',
+    },
+    {
+        id: 4,
+        pic: <IoFastFoodSharp />,
+        name: 'Chips with Salsa',
+        price: '9.64',
+    },
+    {
+        id: 5,
+        pic: <IoFastFoodSharp />,
+        name: 'Noodles with eggs',
+        price: '15.24',
+    },
+    {
+        id: 6,
+        pic: <IoFastFoodSharp />,
+        name: 'Cappuccino with foam',
+        price: '7.84',
+    },
+    {
+        id: 7,
+        pic: <IoFastFoodSharp />,
+        name: 'Pasta with Broccoli',
+        price: '18.77',
+    },
+    {
+        id: 8,
+        pic: <IoFastFoodSharp />,
+        name: 'Toast with fried eggs',
+        price: '12.48',
+    },
+]
+
 export default class MenuItems extends React.Component {
+    state = {
+        Itemcount: 0,
+        CartSum: 5,
+    }
+    ItemAdd1 = () => {
+        this.setState({
+            Itemcount: this.state.Itemcount + 1,
+            CartSum: this.state.CartSum + 1,
+        })
+    }
+
+    ItemRemove1 = () => {
+        this.setState({
+            Itemcount: this.state.Itemcount - 1,
+        })
+    }
+
+    ItemAdd2 = () => {
+        this.setState({
+            Itemcount: this.state.Itemcount + 1,
+            CartSum: this.state.CartSum + 1,
+        })
+    }
+
+    ItemRemove2 = () => {
+        this.setState({
+            Itemcount: this.state.Itemcount - 1,
+        })
+    }
+
+    ItemAdd3 = () => {
+        this.setState({
+            Itemcount: this.state.Itemcount + 1,
+            CartSum: this.state.CartSum + 1,
+        })
+    }
+
+    ItemRemove3 = () => {
+        this.setState({
+            Itemcount: this.state.Itemcount - 1,
+        })
+    }
+
+    ItemAdd4 = () => {
+        this.setState({
+            Itemcount: this.state.Itemcount + 1,
+            CartSum: this.state.CartSum + 1,
+        })
+    }
+
+    ItemRemove4 = () => {
+        this.setState({
+            Itemcount: this.state.Itemcount - 1,
+        })
+    }
+
+    ItemAdd5 = () => {
+        this.setState({
+            Itemcount: this.state.Itemcount + 1,
+            CartSum: this.state.CartSum + 1,
+        })
+    }
+
+    ItemRemove5 = () => {
+        this.setState({
+            Itemcount: this.state.Itemcount - 1,
+        })
+    }
+
+    ItemAdd6 = () => {
+        this.setState({
+            Itemcount: this.state.Itemcount + 1,
+            CartSum: this.state.CartSum + 1,
+        })
+    }
+
+    ItemRemove6 = () => {
+        this.setState({
+            Itemcount: this.state.Itemcount - 1,
+        })
+    }
+
+    ItemAdd7 = () => {
+        this.setState({
+            Itemcount: this.state.Itemcount + 1,
+            CartSum: this.state.CartSum + 1,
+        })
+    }
+
+    ItemRemove7 = () => {
+        this.setState({
+            Itemcount: this.state.Itemcount - 1,
+        })
+    }
+
+    ItemAdd8 = () => {
+        this.setState({
+            Itemcount: this.state.Itemcount + 1,
+            CartSum: this.state.CartSum + 1,
+        })
+    }
+
+    ItemRemove8 = () => {
+        this.setState({
+            Itemcount: this.state.Itemcount - 1,
+        })
+    }
     render() {
         return (
             <div>
-                <Container>
-                    <br />
-                    <Row>
-                        <Col md={4}>
-                            <span className="ImgStyle">
-                                <IoFastFoodSharp />
-                            </span>
-                            <h1>Sweet Potato Salad</h1>
-                            <h2>Price: $11.11</h2>
-                            <button>
-                                + <BsCartFill /> Add to cart
-                            </button>
-                            <hr />
-                        </Col>
-                        <Col md={4}>
-                            <span className="ImgStyle">
-                                <IoFastFoodSharp />
-                            </span>
-                            <h1>Brown Crust Pizza</h1>
-                            <h2>Price: $11.11</h2>
-                            <button>
-                                + <BsCartFill /> Add to cart
-                            </button>
-                            <hr />
-                        </Col>
-                        <Col md={4}>
-                            <span className="ImgStyle">
-                                <IoFastFoodSharp />
-                            </span>
-                            <h1>Mega Burger with Fries</h1>
-                            <h2>Price: $17.11</h2>
-                            <button>
-                                + <BsCartFill /> Add to cart
-                            </button>
-                            <hr />
-                        </Col>
-                    </Row>
-                    <br />
-                    <br />
+                <span>
+                    {this.state.Itemcount} | {this.state.CartSum}
+                </span>
+                {items.map((item) => (
+                    <Container>
+                        <br />
 
-                    <Row>
-                        <Col md={4}>
+                        <li className="col">
                             <span className="ImgStyle">
-                                <IoFastFoodSharp />
+                                {item.pic}
+                                <br />
                             </span>
-                            <h1>Chips with Salsa</h1>
-                            <h2>Price: $9.64</h2>
-                            <button>
-                                + <BsCartFill /> Add to cart
-                            </button>
-                            <hr />
-                        </Col>
-                        <Col md={4}>
-                            <span className="ImgStyle">
-                                <IoFastFoodSharp />
-                            </span>
-                            <h1>Noodles with eggs</h1>
-                            <h2>Price: $15.24</h2>
-                            <button>
-                                + <BsCartFill /> Add to cart
-                            </button>
-                            <hr />
-                        </Col>
-                        <Col md={4}>
-                            <span className="ImgStyle">
-                                <IoFastFoodSharp />
-                            </span>
-                            <h1>Cappuccino with foam</h1>
-                            <h2>Price: $7.84</h2>
-                            <button>
-                                + <BsCartFill /> Add to cart
-                            </button>
-                            <hr />
-                        </Col>
-                    </Row>
+                            {item.name}
+                            <br />
+                            {item.price}
+                            <br />
 
-                    <br />
-                    <br />
+                            <button onClick={this.ItemAdd1}>+</button>
 
-                    <Row>
-                        <Col md={4}>
-                            <span className="ImgStyle">
-                                <IoFastFoodSharp />
-                            </span>
-                            <h1>Pasta with Broccoli</h1>
-                            <h2>Price: $18.77</h2>
-                            <button>
-                                + <BsCartFill /> Add to cart
-                            </button>
-                            <hr />
-                        </Col>
-                        <Col md={4}>
-                            <span className="ImgStyle">
-                                <IoFastFoodSharp />
-                            </span>
-                            <h1>Toast with fried eggs</h1>
-                            <h2>Price: $12.48</h2>
-                            <button>
-                                + <BsCartFill /> Add to cart
-                            </button>
-                            <hr />
-                        </Col>
-                    </Row>
+                            <span className="currentcounts">Add to cart</span>
 
-                    <br />
-                </Container>
+                            <button onClick={this.ItemRemove1}>-</button>
+                            <hr />
+                        </li>
+                    </Container>
+                ))}
             </div>
         )
     }
